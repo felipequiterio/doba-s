6a0ff7dc727729511db7e3144a431e84cbeea0cd
Felipe Quiterio <felipe.quiterio@ip.tv>
2024-10-15 02:03:36 -0400

    Implementing logic tohandle query routing

---
+++ llm/query.py
@@ -0,0 +1,49 @@
+import os
+import ollama
+from dotenv import load_dotenv
+from utils.log import get_custom_logger
+
+logger = get_custom_logger("QUERY")
+
+load_dotenv()
+MODEL = os.getenv("MODEL")
+
+
+def route(message):
+    logger.info("Routing message")
+    system = {
+        "role": "system",
+        "content": """
+            You are an intelligent assistant capable of routing queries. 
+            If the user's query is conversational, route to conversational agent. 
+            If it requires an action, route to the tool agent.
+            """,
+    }
+
+    user = {"role": "user", "content": message}
+
+    response = ollama.chat(
+        model=MODEL,
+        messages=[system, user],
+        tools=[
+            {
+                "type": "function",
+                "function": {
+                    "name": "route_query",
+                    "description": "Route the query to a specific agent",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "agent": {
+                                "type": "string",
+                                "description": "Type of the agent: conversational, tool.",
+                            },
+                        },
+                        "required": ["agent"],
+                    },
+                },
+            },
+        ],
+    )
+
+    return response
