45fd8f984c7276078fb549f8ed6d2ec4627740ef
Felipe Quiterio <felipe.quiterio@ip.tv>
2024-12-18 10:17:26 -0500

    Improving ollama invoke logic

--- llm/invoke.py
+++ llm/invoke.py
@@ -1,23 +1,25 @@
 import ollama
 from openai import OpenAI
-from config import OLLAMA_MODEL, OPENAI_MODEL
 from llm import get_arguments
 from utils.log import get_custom_logger
-
+from config import OLLAMA_MODEL, OPENAI_MODEL
 
 logger = get_custom_logger("INVOKE")
 
 client = OpenAI()
 
 
-def ollama_invoke(system_message: dict, user_message: dict, payload: dict) -> dict:
+def ollama_invoke(system_message: str, user_message: str, payload: dict) -> dict:
     tools = None
     if payload:
         tools = [{"type": "function", "function": payload}]
 
-    response = ollama.chat(
-        model=OLLAMA_MODEL, messages=[system_message, user_message], tools=tools
-    )
+    messages = [
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": user_message},
+    ]
+
+    response = ollama.chat(model=OLLAMA_MODEL, messages=messages, tools=tools)
 
     if payload:
         response = get_arguments(response)
